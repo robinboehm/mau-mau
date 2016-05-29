a5a1a6fd065811e864c324dff6811b02f0d7d205
Robin Böhm <robin.boehm@symetics.com>
2016-05-29 13:29:11 +0200

    expand draw card on 7 rule

--- src/app/mau-mau.component.ts
+++ src/app/mau-mau.component.ts
@@ -5,29 +5,30 @@ import {DeckComponent} from "./deck/deck.component";
 import {PileComponent} from "./pile/pile.component";
 
 
-
 @Component({
   moduleId: module.id,
   selector: 'mau-mau-app',
   templateUrl: 'mau-mau.component.html',
   styleUrls: ['mau-mau.component.css'],
-  directives: [CardComponent,HandOfCardsComponent,DeckComponent,PileComponent]
+  directives: [CardComponent, HandOfCardsComponent, DeckComponent, PileComponent]
 })
 export class MauMauAppComponent {
   currentDeck;
   pile;
-  player1Cards;
-  player2Cards;
+  computerCards;
+  playerCards;
   myTurn;
+  cardsToDraw;
 
 
-  constructor(){
-    this.currentDeck= this.shuffle(this.generateCardDeck());
-    this.pile=[];
-    this.player1Cards=this.getPlayersCards(7);
-    this.player2Cards=this.getPlayersCards(7);
+  constructor() {
+    this.currentDeck = this.shuffle(this.generateCardDeck());
+    this.pile = [];
+    this.computerCards = this.getPlayersCards(7);
+    this.playerCards = this.getPlayersCards(7);
     this.drawCard(this.pile);
-    this.myTurn=true;
+    this.myTurn = true;
+    this.cardsToDraw = 0;
   }
 
   generateCardDeck() {
@@ -35,8 +36,8 @@ export class MauMauAppComponent {
     let suits = ['C', 'H', 'S', 'D'];
     let ranks = ['7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
-    for (let suit of suits){
-      for(let rank of ranks){
+    for (let suit of suits) {
+      for (let rank of ranks) {
         let card = {
           rank: rank,
           suit: suit
@@ -47,10 +48,10 @@ export class MauMauAppComponent {
     return cardDeck;
   }
 
-  getPlayersCards(numberOfCards){
+  getPlayersCards(numberOfCards) {
     let playersCards = [];
 
-    for(let i=0; i<numberOfCards;i++){
+    for (let i = 0; i < numberOfCards; i++) {
       playersCards.push(this.currentDeck.pop())
     }
     return playersCards
@@ -58,25 +59,45 @@ export class MauMauAppComponent {
   }
 
 
-  drawCard(destination){
+  drawCard(destination) {
     destination.push(this.currentDeck.pop());
-    this.myTurn=!this.myTurn
+    this.myTurn = !this.myTurn
   }
 
-  discardCard($event){
+  discardCard($event) {
     let card = $event;
-    if(this.isCardValid(card)){
-      this.pile.push(card);
-      this.player2Cards = this.player2Cards.filter((element) => element != card);
-      if(card.rank === '8'){
-        this.myTurn=true;
-      } else if(card.rank === '7'){
-        this.player1Cards.push(this.currentDeck.pop())
-        this.player1Cards.push(this.currentDeck.pop())
-        this.myTurn=true;
+
+    if (this.isCardValid(card)) { // Ist die aktuelle Karte legbar?
+      this.pile.push(card); // Leg die Karte auf dem Pile
+
+      // Nimm die Karte aus meiner Hand
+      this.playerCards = this.playerCards.filter((element) => element != card);
+
+      if (card.rank === '8') { // Wenn wir ne 8 gelegt haben
+        // sind wir wieder dran
+        this.myTurn = true;
+      } else if (card.rank === '7') { // außer die Karte ist eine 7
+        this.cardsToDraw += 2; //wird Zieh-Counter um 2 erhöht
+        // wenn Gegner auch eine 7 hat
+        if (this.containsSeven(this.computerCards)) {
+
+        } else {
+          // wenn Gegner keine 7 hat, dann muss Gegner Karten ziehen
+          for (let i = 0; i < this.cardsToDraw; i++) {
+            this.computerCards.push(this.currentDeck.pop())
+          }
+          // Zieh-Counter wird wieder auf 0 gesetzt
+          this.cardsToDraw = 0;
+          // und wir sind wieder dran
+          this.myTurn = true;
+
+        }
+
+
       }
-      else{
-        this.myTurn=false;
+      else {
+        // In allen anderen Fällen ist der andere normal dran
+        this.myTurn = false;
       }
 
 
@@ -84,45 +105,44 @@ export class MauMauAppComponent {
 
   }
 
-  shuffle(arr){
-      if (!Array.isArray(arr)) {
-        throw new TypeError('Expected an array');
-      }
+  shuffle(arr) {
+    if (!Array.isArray(arr)) {
+      throw new TypeError('Expected an array');
+    }
 
-      var rand;
-      var tmp;
-      var len = arr.length;
-      var ret = arr.slice();
+    var rand;
+    var tmp;
+    var len = arr.length;
+    var ret = arr.slice();
 
-      while (len) {
-        rand = Math.floor(Math.random() * len--);
-        tmp = ret[len];
-        ret[len] = ret[rand];
-        ret[rand] = tmp;
-      }
+    while (len) {
+      rand = Math.floor(Math.random() * len--);
+      tmp = ret[len];
+      ret[len] = ret[rand];
+      ret[rand] = tmp;
+    }
 
-      return ret;
+    return ret;
   }
 
 
-  computerDiscard(){
-    let card = this.player1Cards.pop();
+  computerDiscard() {
+    let card = this.computerCards.pop();
     this.pile.push(card);
-    this.myTurn=true;
+    this.myTurn = true;
   }
 
-  isCardValid(card){
-    let topCard = this.pile[this.pile.length-1];
+  isCardValid(card) {
+    let topCard = this.pile[this.pile.length - 1];
     let isSameSuit = (card.suit === topCard.suit);
     let isSameRank = (card.rank === topCard.rank);
     let isCardJack = card.rank === 'J';
     return (isSameSuit || isSameRank || isCardJack) && this.myTurn;
   }
 
-  containsSeven(cardDeck){
+  containsSeven(cardDeck) {
     return cardDeck.some(card => card.rank === '7');
   }
-
 
 
 }
